e4d733a9bfd6cb73513c8e482b02dec6a111390e
ViniciusKaique <viniciusloma@gmail.com>
2025-08-31 11:28:18 +0000

    ok

---
+++ includes/push_swap.h
@@ -0,0 +1,31 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//struct do circular buffer
+typedef struct s_stack {
+    int *tab;
+    int start;  
+    int size;
+    int capacity;
+} t_stack;
+
+//stack_utils
+t_stack *stack_init(int capacity);
+void    stack_free(t_stack *s);
+void    sa(t_stack *a);
+void    sb(t_stack *b);
+void    ss(t_stack *a, t_stack *b);
+void    pa(t_stack *a, t_stack *b);
+void    pb(t_stack *a, t_stack *b);
+void    ra(t_stack *a);
+void    rb(t_stack *b);
+void    rr(t_stack *a, t_stack *b);
+void    rra(t_stack *a);
+void    rrb(t_stack *b);
+void    rrr(t_stack *a, t_stack *b);
+
+#endif
+
